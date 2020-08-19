73467e92c97ff97af4d5ef04451bc56fd34168cb
vikascowdary47 <vikaschowdary47@gmail.com>
2020-08-19 21:07:13 +0530

    icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,31 @@
 import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
 import "./Sidebar.css";
+import ChatIcon from "@material-ui/icons/Chat";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 const Sidebar = () => {
-  return <h1>siebar</h1>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search"></div>
+      <div className="sidebar__chats"></div>
+    </div>
+  );
 };
 
 export default Sidebar;
