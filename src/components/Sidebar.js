cbfdbe460bfdb260803ab0c143550f01cbc2012e
vikascowdary47 <vikaschowdary47@gmail.com>
2020-08-19 20:24:18 +0530

    first commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  return <h1>siebar</h1>;
+};
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB94cGVWK-RTPKewGuaDTJMyPoo3DkL0pQ",
+  authDomain: "what-s-app-clone-de678.firebaseapp.com",
+  databaseURL: "https://what-s-app-clone-de678.firebaseio.com",
+  projectId: "what-s-app-clone-de678",
+  storageBucket: "what-s-app-clone-de678.appspot.com",
+  messagingSenderId: "498085701849",
+  appId: "1:498085701849:web:12775f41756062d5beb6c8",
+  measurementId: "G-NYFK7MMTE6",
+};
